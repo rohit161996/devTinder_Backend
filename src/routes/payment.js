06591e77077c00fd4b45572bcbebfede8ff83fc8
rohitramchandanistm1996@gmail.com <Radha@1234567890->
2025-10-04 03:43:03 +0530

    Payment Verification API added

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -98,4 +98,17 @@ paymentRouter.post(
     }
 );
 
+paymentRouter.get(
+    "/premium/verify",
+    userAuth,
+    async (req, res) => {
+        const user = req.user;
+        if (user.isPremium) {
+            return res.json({ isPremium: true });
+        }
+
+        return res.json({ isPremium: false });
+    }
+);
+
 module.exports = paymentRouter;
